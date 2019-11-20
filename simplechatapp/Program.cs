ae1e44a749c15d5c080815b9287dc4a96391d6fd
quang nhut <quangnhutcntt@gmail.com>
2019-11-20 11:15:55 +0700

    aaa

--- simplechatapp/Program.cs
+++ simplechatapp/Program.cs
@@ -14,7 +14,7 @@ namespace simplechatapp
         [STAThread]
         static void Main()
         {
-            //abc
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
